1023d63ae322cb7cedfca732c3b88dbb739c31a1
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-30 17:08:29 +0300

    chore: sql connection

--- encrypt-database/Form1.cs
+++ encrypt-database/Form1.cs
@@ -7,22 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace encrypt_database
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
-
+        SqlConnection connect = new SqlConnection(@"Data Source=DESKTOP-LRMEISB\SQLEXPRESS; Initial Catalog=Encrypt; Integrated Security=True");
         private void btnSave_Click(object sender, EventArgs e)
         {
-            string text = txtName.Text;
-            byte[] dataArr = ASCIIEncoding.ASCII.GetBytes(text);
-            string encryptData= Convert.ToBase64String(dataArr);
-            label6.Text=encryptData;
+            string name = txtName.Text;
+            byte[] nameArr = ASCIIEncoding.ASCII.GetBytes(name);
+            string encryptName = Convert.ToBase64String(nameArr);
+            label6.Text = encryptName;
         }
     }
 }
